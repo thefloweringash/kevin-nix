547ce96c45bd64db9033cdcf27d1409cc2d2aa7e
Andrew Childs <lorne@cons.org.nz>
2018-02-21 23:38:35 +0900

    Update for recent changes to nixpkgs

--- packages/linux_gru_4_4.nix
+++ packages/linux_gru_4_4.nix
@@ -1,18 +1,29 @@
-{ stdenv, runCommand, fetchgit, buildLinux, kernelPatches ? [] }:
+{ stdenv, hostPlatform, runCommand, fetchgit, linuxManualConfig,
+  features ? {}, kernelPatches ? [] }:
 
-buildLinux {
-  inherit stdenv kernelPatches;
+# Additional features cannot be added to this kernel
+assert features == {};
 
-  src = fetchgit {
-    url = "https://chromium.googlesource.com/chromiumos/third_party/kernel/";
-    rev = "3aa6760c93900123744c104b67fecda917f73fde";
-    sha256 = "0ff4qzw5mjz93v6midrg1g82v75nvqf40nk86zgqabbij58zwjy4";
+let
+  passthru = { features = {}; };
+
+  drv = linuxManualConfig {
+    inherit stdenv kernelPatches hostPlatform;
+
+    src = fetchgit {
+      url = "https://chromium.googlesource.com/chromiumos/third_party/kernel/";
+      rev = "3aa6760c93900123744c104b67fecda917f73fde";
+      sha256 = "0ff4qzw5mjz93v6midrg1g82v75nvqf40nk86zgqabbij58zwjy4";
+    };
+
+    version = "4.4.110-gru";
+    modDirVersion = "4.4.110-ARCH";
+
+    configfile = ./linux-gru.config;
+
+    allowImportFromDerivation = true; # Let nix check the assertions about the config
   };
 
-  version = "4.4.110-gru";
-  modDirVersion = "4.4.110-ARCH";
+in
 
-  configfile = ./linux-gru.config;
-
-  allowImportFromDerivation = true; # Let nix check the assertions about the config
-}
+stdenv.lib.extendDerivation true passthru drv
