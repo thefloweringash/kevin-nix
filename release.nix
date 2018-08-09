8e3cd698f0819e4d9f9ee2c96afbe2d8a4e69c61
Andrew Childs <lorne@cons.org.nz>
2018-08-10 00:55:49 +0900

    Borrow some magic from nixos/release-combined.nix for image names

--- release.nix
+++ release.nix
@@ -1,5 +1,19 @@
-with (import <nixpkgs/nixos> {
-  configuration = ./sd-image-kevin.nix;
+{ nixpkgs ? { outPath = <nixpkgs>; revCount = 56789; shortRev = "gfedcba"; }
+, stableBranch ? false
+}:
+
+let
+  versionSuffix =
+    (if stableBranch then "." else "pre") + "${toString nixpkgs.revCount}.${nixpkgs.shortRev}";
+
+  versionModule =
+    { system.nixos.versionSuffix = versionSuffix;
+      system.nixos.revision = nixpkgs.rev or nixpkgs.shortRev;
+    };
+in
+
+with (import (nixpkgs+"/nixos/lib/eval-config.nix") {
+  modules = [ ./sd-image-kevin.nix versionModule ];
 });
 
 {
