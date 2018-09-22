32a6018d82058c9454f9a7d1192962a55494e580
Andrew Childs <lorne@cons.org.nz>
2018-09-22 15:04:12 +0900

    panfrost: specify sha256 hashes

--- packages/panfrost.nix
+++ packages/panfrost.nix
@@ -1,4 +1,5 @@
 { stdenv
+, fetchgit
 , mesa_drivers
 , meson, ninja
 , bison, flex
@@ -11,19 +12,19 @@ mesa_drivers.overrideAttrs (o: {
   patches = [];
   postInstall = ":";
   outputs = ["out"];
-  src = builtins.fetchGit {
+  src = fetchgit {
     name = "mesa";
     url = https://gitlab.freedesktop.org/panfrost/mesa;
-    rev = "13aae958f70a69166c0c241f683c8d3566ed26db";
-    ref = "upstream-overlay";
+    rev = "13aae958f70a69166c0c241f683c8d3566ed26db"; # upstream-overlay
+    sha256 = "056smp4nkxcml1phmf4wspygjlrrjcnh1zykm634qfw91c0mizxk";
   };
   preConfigure = ''
     rm src/gallium/drivers/panfrost
-    ln -s ${builtins.fetchGit {
+    ln -s ${fetchgit {
       name = "panfrost";
       url = https://gitlab.freedesktop.org/panfrost/panfrost;
-      rev = "a25f2c0938874d71676c193aa31c20f99a7dcdf5";
-      ref = "incremental-t8xx";
+      rev = "a25f2c0938874d71676c193aa31c20f99a7dcdf5"; # incremental-t8xx
+      sha256 = "1l6kz67m9cm2lhi8yb41vlb3ayva5li6r000zvpj2ffkq623lhqq";
     }} src/gallium/drivers/panfrost
   '';
 })
