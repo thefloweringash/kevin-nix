8e30e940288db7dc9bf7fcabb8e5c0ae0b5d24d7
Andrew Childs <lorne@cons.org.nz>
2018-09-22 14:54:00 +0900

    mali: add enable flag for gldriver

--- modules/mali.nix
+++ modules/mali.nix
@@ -1,9 +1,25 @@
 { config, lib, pkgs, ... }:
 
+let
+  cfg = config.hardware;
+in
 {
-  services.udev.extraRules = ''
-    KERNEL=="mali[0-9]", MODE="0660", GROUP="video"
-  '';
+  options = {
+    hardware.libmali.enable  = lib.mkEnableOption "Enable libmali gl driver";
+  };
 
-  hardware.opengl.package = pkgs.rockchip-linux-libmali-gldriver;
+  config = lib.mkMerge [
+    {
+      # TODO: this should probably be integrated with login sessions.
+      services.udev.extraRules = ''
+        KERNEL=="mali[0-9]", MODE="0660", GROUP="video"
+      '';
+
+      environment.systemPackages = with pkgs; [ glxinfo ];
+    }
+    (lib.mkIf cfg.libmali.enable {
+      # This is wishful thinking until the versions match
+      hardware.opengl.package = pkgs.rockchip-linux-libmali-gldriver;
+    })
+  ];
 }
