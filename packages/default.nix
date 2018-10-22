47155bd93c9fd8ea4055c3ccfeff66d355904494
Andrew Childs <lorne@cons.org.nz>
2018-10-22 23:18:03 +0900

    linux_gru_4_4: make package more self contained

--- packages/default.nix
+++ packages/default.nix
@@ -1,13 +1,13 @@
 self: super: {
-  linux_gru_4_4 = super.callPackage ./linux_gru_4_4.nix {
+  linux_gru_4_4 = super.callPackage ./linux_gru_4_4 {
     kernelPatches = with self; [
       kernelPatches.bridge_stp_helper
       kernelPatches.cpu-cgroup-v2."4.4"
       kernelPatches.modinst_arg_list_too_long
 
-      { name = "arch-1"; patch = ./0001-Input-atmel_mxt_ts-Use-KERN_DEBUG-loglevel-for-statu.patch; }
-      { name = "arch-2"; patch = ./0002-Revert-CHROMIUM-drm-rockchip-Add-PSR-residency-debug.patch; }
-      { name = "arch-3"; patch = ./0003-temporary-hack-to-fix-console-output.patch; }
+      { name = "arch-1"; patch = ./linux_gru_4_4/0001-Input-atmel_mxt_ts-Use-KERN_DEBUG-loglevel-for-statu.patch; }
+      { name = "arch-2"; patch = ./linux_gru_4_4/0002-Revert-CHROMIUM-drm-rockchip-Add-PSR-residency-debug.patch; }
+      { name = "arch-3"; patch = ./linux_gru_4_4/0003-temporary-hack-to-fix-console-output.patch; }
     ];
   };
 
