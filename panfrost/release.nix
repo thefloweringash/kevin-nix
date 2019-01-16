f516069fd75c9f03b1b2b1f27f841047f726d57c
Andrew Childs <lorne@cons.org.nz>
2019-01-16 10:23:17 +0900

    panfrost: allow externally specified mali_kbase source

--- panfrost/release.nix
+++ panfrost/release.nix
@@ -1,12 +1,18 @@
 { nixpkgs ? { outPath = <nixpkgs>; }
 , stableBranch ? false
 , panfrost ? null
+, mali_kbase ? null
 }:
 
 let
   panfrostSuffix =
     if panfrost ? revCount && panfrost ? shortRev
     then "-panfrost${toString panfrost.revCount}.${panfrost.shortRev}"
+    else "";
+
+  mali_kbaseSuffix =
+    if mali_kbase ? revCount && mali_kbase ? shortRev
+    then "-${toString mali_kbase.revCount}.${mali_kbase.shortRev}"
     else "";
 
   nixSuffix =
@@ -32,6 +38,18 @@ let
         };
       })];
     } else {};
+
+  mali_kbaseSrcModule = if mali_kbase != null then
+    {
+      nixpkgs.overlays = [(self: super: {
+        linuxPackagesFor = kernel: (super.linuxPackagesFor kernel).extend (kself: ksuper: {
+          mali_kbase = ksuper.mali_kbase.overrideAttrs ({ pname, ... }: {
+            name = "${pname}${mali_kbaseSuffix}";
+            src = mali_kbase;
+          });
+        });
+      })];
+    } else {};
 in
 
 with (import (nixpkgs+"/nixos/lib/eval-config.nix") {
@@ -41,6 +59,7 @@ with (import (nixpkgs+"/nixos/lib/eval-config.nix") {
     ../modules/mali.nix
     ./live-environment.nix
     panfrostSrcModule
+    mali_kbaseSrcModule
   ];
 });
 
