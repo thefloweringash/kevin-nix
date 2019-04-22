358a1c8684ea33eda2d11ba0218b4eaf243270ae
Andrew Childs <lorne@cons.org.nz>
2019-04-22 21:17:41 +0900

    early-boot: throw in more modules
    I don't know what changed, but the old version was not sufficient to
    make the display power in the initrd environment when upgrading from
    5.0 to 5.0.7.

--- modules/early-boot.nix
+++ modules/early-boot.nix
@@ -7,5 +7,12 @@
     "pwm_bl"
     "pwm_cros_ec"
     "rockchipdrm"
+    "dw_hdmi"
+    "dw_mipi_dsi"
+    "drm"
+    "cros_cs_dev"
+    "cros_ec_ctl"
+    "cros_ec_sensors"
+    "cros_ec_sensors_core"
   ];
 }
